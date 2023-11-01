5846b150d8e60d33d2bb4aaa0eaccb07721bebc7
Svitlana-Hreshnova <133237028+Svitlana-Hreshnova@users.noreply.github.com>
2023-11-01 11:23:43 +0100

    add

--- src/index.js
+++ src/index.js
@@ -8,14 +8,24 @@ const apiKey = '40298326-3542dba9bdb0915da3eae5d6c';
 const perPage = 40;
 let currentPage = 1;
 let searchQuery = '';
+let totalHits = 0;
+let isFetching = false;
 
 $(document).ready(() => {
   const searchForm = $('#search-form');
   const gallery = $('.gallery');
   const loadMoreButton = $('.load-more');
-    
 
-// Обробник подання форми пошуку
+  // Функція для обробки події прокрутки та завантаження додаткових зображень
+  function handleScroll() {
+    if ($(window).scrollTop() + $(window).height() >= $(document).height() - 300 && !isFetching) {
+      isFetching = true;
+      currentPage += 1;
+      searchImages(searchQuery, currentPage);
+    }
+  }
+
+  // Обробник події відправки форми пошуку
   searchForm.submit((event) => {
     event.preventDefault();
     gallery.empty();
@@ -23,18 +33,11 @@ $(document).ready(() => {
     searchQuery = event.target.elements.searchQuery.value.trim();
     searchImages(searchQuery, currentPage);
   });
-    
-    
- // Обробник події прокрутки сторінки
-  $(window).scroll(() => {
-    if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
-      currentPage += 1;
-      searchImages(searchQuery, currentPage);
-    }
-  });
 
-    
-    // Функція для пошуку зображень
+  // Додавання обробника події прокрутки сторінки
+  $(window).on('scroll', handleScroll);
+
+  // Функція для отримання зображень з API Pixabay
   async function searchImages(query, page) {
     const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`;
 
@@ -46,27 +49,35 @@ $(document).ready(() => {
         Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
       } else {
         renderImages(data.hits);
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        totalHits = data.totalHits;
+
+        const totalImagesLoaded = page * perPage;
+        if (totalImagesLoaded >= totalHits) {
+          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+          $(window).off('scroll');
+        } else {
+          Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+          isFetching = false;
+        }
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Помилка:', error);
       Notiflix.Notify.failure('An error occurred while fetching images.');
+      isFetching = false;
     }
   }
 
-    
-    // Функція для відображення зображень
+  // Функція для відображення зображень у галереї
   function renderImages(images) {
     images.forEach((image) => {
       const photoCard = $('<div class="photo-card"></div>');
-
       const link = $(`<a href="${image.largeImageURL}" title="${image.tags}" data-lightbox="gallery"></a>`);
       const img = $(`<img src="${image.webformatURL}" alt="${image.tags}" loading="lazy">`);
       const info = $('<div class="info"></div>');
-      const likes = createInfoItem('Likes', image.likes);
-      const views = createInfoItem('Views', image.views);
-      const comments = createInfoItem('Comments', image.comments);
-      const downloads = createInfoItem('Downloads', image.downloads);
+      const likes = createInfoItem('Лайки', image.likes);
+      const views = createInfoItem('Перегляди', image.views);
+      const comments = createInfoItem('Коментарі', image.comments);
+      const downloads = createInfoItem('Завантаження', image.downloads);
 
       info.append(likes, views, comments, downloads);
       link.append(img);
@@ -78,8 +89,7 @@ $(document).ready(() => {
     lightbox.refresh();
   }
 
-    
-     // Функція для створення блоку інформації про зображення
+  // Функція для створення блоку інформації про зображення
   function createInfoItem(label, value) {
     return $(`<p class="info-item"><b>${label}:</b> ${value}</p>`);
   }
